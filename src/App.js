a1eec0e81cb74ee1679df215a50a467522b7bd7d
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-06-05 17:22:44 -0600

    updated raact-routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Chat from './componets/Chat';
 import Register from './componets/Register';
 import Login from './componets/Login';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route,  Routes  } from 'react-router-dom';
 
 
 function App() {
@@ -11,11 +11,11 @@ function App() {
   return (
     <div className="App">
     <h1>Don't Stop</h1>
-    <Router>
+    <Routes>
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
       <Route path="/chat" component={Chat} />
-    </Router>
+      </Routes>
   </div>
   );
   }
