d50189918fa0c607008547cb85482f0fb4307064
Memo Diaz <121352998+memodiaz44@users.noreply.github.com>
2023-06-05 20:13:29 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import './App.css';
 import Chat from './componets/Chat';
 import Register from './componets/Register';
 import Login from './componets/Login';
-import { Route,  Routes  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
-
-
   return (
-    <div className="App">
-    <h1>Don't Stop</h1>
-    <Routes>
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={Login} />
-      <Route path="/chat" component={Chat} />
-      </Routes>
-  </div>
+    <Router>
+      <div className="App">
+        <h1>Don't Stop</h1>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </div>
+    </Router>
   );
-  }
+}
 
 export default App;
